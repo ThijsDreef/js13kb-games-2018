6f1ef5b91ed321f2ea928fae685c2d603f6a49da
Thijs <22070@ma-web.nl>
2018-08-15 22:01:36 +0000

    added buffer.js

---
+++ src/webgl/Buffer.js
@@ -0,0 +1,27 @@
+const AB = 0x8892;
+const SD = 0x88E4;
+class Buffer {
+  constructor(gl, data) {
+    this._gl = gl;
+    this.id = gl.createBuffer();
+    this.buffer(data);
+  }
+
+  bind() {
+    this._gl.bindBuffer(AB, this.id);
+  }
+
+  buffer(data){
+    this.bind();
+    this._gl.bufferData(AB, data, SD);
+  }
+
+  point(shader, attrib, size, type) {
+    this.bind();
+    const attribLocation = shader.getAttrib(attrib);
+    this.gl.enableVertexAttribArray(attribLocation);
+    this.gl.vertexAttribPointer(attribLocation, size, type, false, 0, 0);
+  }
+}
+
+export default Buffer;
