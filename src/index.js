f023d31f3564b55412e3210e32f9af30f15f0ca3
dice9907 <22070@ma-web.nl>
2018-08-22 00:03:55 +0200

    added some generation and lighting

--- src/index.js
+++ src/index.js
@@ -27,44 +27,56 @@ const gl = canvas.getContext("webgl", {preserveDrawingBuffer: true});
 const shader = new Shader(gl, frag, vert, {attribs: ['aPosition'], uniforms: ['uCamera', 'cameraPos']});
 gl.enable(gl.DEPTH_TEST);
 const positions = [];
+const grid = [];
+for (let i = 0; i < 10; i++) {
+  const subGrid = [];
+  for (let j = 0; j < 10; j++) {
+    subGrid.push(false);
+  }
+  grid.push(subGrid);
+}
+addLayerToGrid(grid, 0.2, -1);
 
- for (let i = 0; i < 100; i ++)
-   positions.push([Math.floor((Math.random() * 2 - 1) * 5), -1, Math.floor((Math.random() * 2 - 1) * 5)]);
-
+function addLayerToGrid(grid, thresHold, layer) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === false && Math.random() > thresHold) {
+        positions.push([i, layer, j]);
+        grid[i][j] = true;
+      }
+    }
+  }
+  if (layer < 2) addLayerToGrid(grid, 0, layer + 1);
+}
 const cubeBuffer = new CubeGeometry(gl, positions);
 
 gl.clearColor(1, 1, 1, 1);
-gl.clear(gl.COLOR_BUFFER_BIT);
-shader.bind();
-shader.uploadMat4(matrix.m,'uCamera');
-cubeBuffer.getBuffer().bind();
-cubeBuffer.getBuffer().point(shader, 'aPosition', 3, gl.FLOAT);
 const p = new Matrix();
 p.perspective(45, window.innerWidth / window.innerHeight, 0.001, 100);
-console.log(p);
+
 draw();
 
 function draw() {
   controls.moveHandler();
   matrix.identity();
+  const a = controls.getPos();
 
   matrix = matrix.rotateX(controls.getRot()[0]);
   matrix = matrix.rotateY(controls.getRot()[1]);
-  matrix = matrix.translate(controls.getPos());
+  matrix = matrix.translate(a);
 
   matrix = p.multiply(matrix);
-  
+
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  
+
   shader.bind();
   shader.uploadMat4(matrix.m,'uCamera');
-  const a = controls.getPos();
   shader.uploadVec3([-a[0], -a[1], -a[2]], 'cameraPos');
-  
+
   cubeBuffer.getBuffer().bind();
   cubeBuffer.getBuffer().point(shader, 'aPosition', 3, gl.FLOAT);
-  
+
   gl.drawArrays(gl.TRIANGLES, 0, cubeBuffer._vertices.length / 3);
-  
+
   requestAnimationFrame(draw);
 }
--- src/shaders/main.frag
+++ src/shaders/main.frag
@@ -5,5 +5,10 @@ varying vec3 inputPos;
 uniform vec3 cameraPos;
 
 void main() {
-  gl_FragColor = clamp(length(inputPos - cameraPos) , 0., 1.) * vec4(inputPos, 1.);
+  float x = mod(inputPos.x, 1.);
+  float y = mod(inputPos.y, 0.5);
+  float z = mod(inputPos.z, 1.);
+  y = 1. - y;
+  gl_FragColor.xyzw = vec4(1. - length(cameraPos - inputPos) / 2., 0, 0, 1.);
+
 }
