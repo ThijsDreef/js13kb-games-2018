bb70a43ae910e44536e4687c79b39f9e59232cf1
Thijs <22070@ma-web.nl>
2018-08-23 19:42:54 +0000

    added some dubgging for terrain collisions

--- src/physics/Physics.js
+++ src/physics/Physics.js
@@ -3,6 +3,27 @@ class Physics {
     constructor() {
         this._colliders = [];
         this._terrain = [];
+	this._p = document.createElement('p');
+	this._p.style.position = 'absolute';
+	this._p.style.top = '0';
+	document.body.appendChild(this._p);
+    }
+
+    _debug(playerPos) {
+	let string = '';
+	for (let x = 0; x < this._terrain.length; x++) {
+	  for(let y = 0; y < this._terrain[x].length; y++) {
+	    if (x == playerPos[0] && playerPos[2] == y)	string += ' p ';
+	    else {
+		if (this._terrain[x][y])
+		  string += ' w ';
+		else
+		  string += ' e ';
+	    }
+	  }
+	  string += '\r\n';
+        }
+	this._p.textContent = string;
     }
 
     generateTerrain(xSize, ySize) {
@@ -12,7 +33,7 @@ class Physics {
             this._terrain.push([]);
             for (let y = 0; y < ySize; y++) {
                 positions.push([x, -1, y]);
-                
+
                 this._terrain[x].push((Math.random() < 0.33));
                 if (this._terrain[x][y]) {
                     positions.push([x, 1, y]);
@@ -29,11 +50,12 @@ class Physics {
         const y = -Math.floor(0.5 + position[2]);
         const oldX = -Math.floor(0.5 + lastPosition[0]);
         const oldY = -Math.floor(0.5 + lastPosition[2]);
-        console.log(oldY - y);
+        this._p.textContent = x + ' ' + y + ' ' + oldX + ' ' + oldY;
+	this._debug([x, 0, y]);
         if (x < 0 || y < 0 || x > this._terrain.length || y > this._terrain[x].length)
             return {hit: false};
-        else 
-            return {hit: this._terrain[x][y], collision: [oldX - x, oldY - y]};
+        else
+            return {hit: (this._terrain[x][y] && (this._terrain[oldX][y] || this._terrain[x][oldY])), collision: [oldX - x, oldY - y]};
     }
 }
 
